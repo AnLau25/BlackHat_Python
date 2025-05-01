7496e059104b363a39c601c6300b5a7fdf98c4bf
AnLau25 <avald024@uottawa.ca>
2025-05-01 10:43:36 -0400

    exec

--- netcat.py
+++ netcat.py
@@ -2,8 +2,17 @@
 import argparse
 import socket 
 import shlex
+import subprocess #interface for process creation and client program interaction
 import sys
 import textwrap
 import threading
+
+def execute(cdm):
+    cdm = cdm.strip
+    if not cdm:
+        return
+    output = subprocess.check_output(shlex.split(cdm), stderr=subprocess.STDOUT)
+    #𝗰𝗵𝗲𝗰𝗸_𝗼𝘂𝘁𝗽𝘂𝘁 runs the comand and captures the output
+    return output.decode()
 
 
