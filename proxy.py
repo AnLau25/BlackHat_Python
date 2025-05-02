c6ea408040730a2d888e1e574d1adbea64b20f67
AnLau25 <avald024@uottawa.ca>
2025-05-02 14:13:11 -0400

    hexdump

---
+++ proxy.py
@@ -0,0 +1,29 @@
+#technicaly, wireshark for when u don't have accces
+import sys
+import socket
+import threading
+
+HEX_FILTER = ''.join(
+    [(len(redr(chr(i)))==3) and chr(i) or '.' for i in range(256)] #If length == 3, get char, else, '.'
+)#String containing ASCII printable characters.
+ #If not printable, print dot
+
+def hexdump(src, length=16, show=True): #Displays coms between local and remote machines
+    if isinstance(src,bytes):
+        src.decode() #Decodes if any byte was passed
+    
+    results = list()
+    
+    for i in range(0, len(src), length): #Grabs pieces of the decode and places it in word
+        word = str(src[i:i+length])
+        
+        printable = word.translate(HEX_FILTER) #Substitutes the strings by the raw printable, via .translate()
+        hexa = ''.join([f'{ord(c):02X}' for c in word]) #Substitutes the raw printable by hex
+        hexwidth =  length*3 
+        results.append(f'{i:04x} {hexa:<{hexwidth}>} {printable}')#New array containg the hex value of the first index, the hex value of the word and the word 
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
+    #Usefull to find user credentials in plain text protocol and understanding unkown protocols
