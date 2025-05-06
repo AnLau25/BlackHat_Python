709919c64932a11a56d5a9d6e16f9a37df9e9744
AnLau25 <avald024@uottawa.ca>
2025-05-06 14:59:42 -0400

    port parsed to int

--- ch2_basicNetworkTools/ssh_cdm.py
+++ ch2_basicNetworkTools/ssh_cdm.py
@@ -2,13 +2,13 @@
 import paramiko
 
 #connects to a SSH server and runs a single command
-def ssh_command(ip, port, user, passwd, cdm):
+def ssh_command(ip, port, user, passwd, cmd):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
     #Set the policy to accept the SSH key for the server (cause we are both)
     client.connect(ip, port=port, username=user, password=passwd)
     
-    _, stdout, stderr = client.exec_command(cdm) #If the connection is succesfull we run the command 
+    _, stdout, stderr = client.exec_command(cmd) #If the connection is succesfull we run the command 
     output = stdout.readlines() + stderr.readlines() 
     if output: #If output, print each line of the output
         print('--- Output ---')
@@ -21,11 +21,10 @@ if __name__ == '__main__':
     user = input('Username: ') #asks on commandline since it is diferent on both machines
     password = getpass.getpass() #gets the password, does no display it
         
-    ip = input('Enter server IP: ') or '192.168.1.203' #testing, could be 127.0.0.1??
-    port = input('Enter port or <CR>: ') or '2222'
-    cdm = input('Enter command or <CR>: ') or 'id'
-    ssh_command(ip, port, user, password, cdm)
-        
+    ip = input('Enter server IP: ') or '192.168.1.203' #testing, should be 127.0.0.1??
+    port = int(input('Enter port or <CR>: ') or 2222)
+    cmd = input('Enter command or <CR>: ') or 'id'
+    ssh_command(ip, port, user, password, cmd)
         
 # 𝗣𝗮𝗿𝗮𝗺𝗶𝗸𝗼
 # - Supports both password and SSH key authentication
