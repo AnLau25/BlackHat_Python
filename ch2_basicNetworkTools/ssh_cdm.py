beb68f687be6b0dfc858014920c60d8e678d7074
AnLau25 <avald024@uottawa.ca>
2025-05-06 14:14:36 -0400

    cmntd

--- ch2_basicNetworkTools/ssh_cdm.py
+++ ch2_basicNetworkTools/ssh_cdm.py
@@ -1,23 +1,25 @@
 #SSH (Secure Shell) protocol on Python with Paramiko (PyCripto for SSH2)
 import paramiko
 
+#connects to a SSH server and runs a single command
 def ssh_command(ip, port, user, passwd, cdm):
     client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    #Set the policy to accept the SSH key for the server (cause we are both)
     client.connect(ip, port=port, username=user, password=passwd)
     
-    _, stdout, stderr = client.exec_command(cdm)
-    output = stdout.readlines() + stderr.readlines()
-    if output:
+    _, stdout, stderr = client.exec_command(cdm) #If the connection is succesfull we run the command 
+    output = stdout.readlines() + stderr.readlines() 
+    if output: #If output, print each line of the output
         print('--- Output ---')
         for line in output:
             print(line.strip())
     
     if __name__ == '__main__':
-        import getpass
+        import getpass #gets current environement username
         # user = getpass.getuser()
-        user = input('Username: ')
-        password = getpass.getpass()
+        user = input('Username: ') #asks on commandline since it is diferent on both machines
+        password = getpass.getpass() #gets the password, does no display it
         
         ip = input('Enter server IP: ') or '192.168.1.203' #testing, could be 127.0.0.1??
         port = input('Enter port or <CR>: ') or '2222'
@@ -25,6 +27,10 @@ def ssh_command(ip, port, user, passwd, cdm):
         ssh_command(ip, port, user, password, cdm)
         
         
-        
-        
+# 𝗣𝗮𝗿𝗮𝗺𝗶𝗸𝗼
+# - Supports both password and SSH key authentication
+# - In irl circumstances, you'd use keys        
+
+# 𝗡𝗼𝘁𝗮𝘀:
+# Wtf is shoulder-surfing???
     
