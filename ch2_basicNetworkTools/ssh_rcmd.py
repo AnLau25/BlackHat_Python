ff9b4166037f7d7f42aed1f55af7a43fe5e0a9d5
AnLau25 <avald024@uottawa.ca>
2025-05-07 20:23:57 -0400

    ssh_command

--- ch2_basicNetworkTools/ssh_rcmd.py
+++ ch2_basicNetworkTools/ssh_rcmd.py
@@ -1,3 +1,30 @@
-#ssh_cdm but it allows multiple commands
+#ssh_cmd but it runs on wndows client
 import paramiko
+import shlex
+import subprocess
 
+#connects to a SSH server and runs a single command
+def ssh_command(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    #Set the policy to accept the SSH key for the server (cause we are both)
+    client.connect(ip, port=port, username=user, password=passwd)
+    
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+    
+    print(ssh_session.recv(1024).decode())
+    while True:
+        command = ssh_session.recv(1024)
+        try:
+            cmd = command.decode()
+            if cmd == 'exit':
+                client.close()
+                break
+            cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+            ssh_session.snde(cmd_output or 'okay')
+        except Exception as e:
+            ssh_session.send(str(e))
+    return
+        
