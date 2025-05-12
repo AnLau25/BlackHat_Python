c99c5e29dcc40c4e2635f6ec80ba60a9a71270c4
AnLau25 <avald024@uottawa.ca>
2025-05-12 18:24:39 -0400

    re-name

--- ch2_basicNetworkTools/rforward.py
+++
@@ -1,8 +0,0 @@
-# 𝗦𝗦𝗛 𝗧𝘂𝗻𝗲𝗹𝗶𝗻𝗴
-# - Instead of directly sending commands, the tunel sends SSH packaged trafic to the server
-# - The server then unpackages it and delivers it
-# - 𝗨𝘀𝗲𝗳𝘂𝗹 𝗶𝗳 𝘁𝗵𝗲 𝗦𝗦𝗛 𝘀𝗲𝗿𝘃𝗲𝗿 𝗵𝗮𝘀 𝗮𝗰𝗰𝗲𝘀 𝘁𝗼 𝘁𝗵𝗲 𝘄𝗲𝗯 𝗯𝘂𝘁 𝗻𝗼 𝘁𝗼𝗼𝗹𝘀 𝘁𝗼 𝗵𝗮𝗰𝗸 𝘄𝗶𝘁𝗵 𝗮𝗻𝗱 𝘂 𝗵𝗮𝘃𝗲 𝗻𝗼 𝘄𝗲𝗯 𝗮𝗰𝗰𝗲𝘀
-# - Ie you are sending the tools through the tunel to the server, to the web
-# - Instead of sending them from the server to the web urself (𝗳𝗼𝗿𝘄𝗮𝗿𝗱 𝗦𝗦𝗛 𝘁𝘂𝗻𝗲𝗹𝗶𝗻𝗴)
-# - 𝗜𝗳 𝗻𝗼 𝗦𝗦𝗛 𝘀𝗲𝗿𝘃𝗲𝗿, 𝗿𝗲𝘃𝗲𝗿𝘀𝗲 𝘁𝘂𝗻𝗻𝗲𝗹𝗶𝗻𝗴 is the option
-# - In that case, u get tuneled to the local host (port to 3389 for ex to acces internal sytem)
