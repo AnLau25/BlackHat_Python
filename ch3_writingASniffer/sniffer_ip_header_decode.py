2ccbcf4f4e81be4b8f7a59c80bc010c703f0ede9
AnLau25 <avald024@uottawa.ca>
2025-05-18 21:16:39 -0400

    ctype_parser

--- ch3_writingASniffer/sniffer_ip_header_decode.py
+++ ch3_writingASniffer/sniffer_ip_header_decode.py
@@ -1,4 +1,4 @@
-/*
-
-
-*/
+# 𝗚𝗲𝘁𝘁𝗶𝗻𝗴 𝗲𝘅𝘁𝗲𝗿𝗻𝗮𝗹 𝗱𝗮𝘁𝗮 𝗶𝗻𝘁𝗼 𝗮 𝗱𝗮𝘁𝗮 𝘀𝘁𝗿𝘂𝗰𝘁𝘂𝗿𝗲 𝘄𝗶𝘁𝗵 𝗽𝘆𝘁𝗵𝗼𝗻:
+#   Ctypes module:  Foreign python library providing a bridge to c-based languages. This enables u to use c-compatible data types and call functions in shared libraries. 
+#                   Ie, main for data types and additional funtionalities.
+#   Struct: Converts between python values and C structs represented as python byte objs. Ie main for handdling binary data. 
