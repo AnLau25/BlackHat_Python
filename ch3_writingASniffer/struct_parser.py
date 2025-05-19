ed6c6ff74d95ebc2993a8f0869c787c780877b91
AnLau25 <avald024@uottawa.ca>
2025-05-19 15:39:04 -0400

    struct_parser

---
+++ ch3_writingASniffer/struct_parser.py
@@ -0,0 +1,6 @@
+#   Struct module: Converts between python values and C structs represented as python byte objs. Ie main for handdling binary data. 
+import ipaddress
+import struct
+
+class IP:
+    
