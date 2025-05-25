bc228ae10023ed24a1684d00802fac5ca42de23f
AnLau25 <avald024@uottawa.ca>
2025-05-25 12:52:12 -0400

    sniffer ICMP specific

--- ch3_writingASniffer/scanner.py
+++ ch3_writingASniffer/scanner.py
@@ -66,46 +66,46 @@ class Scanner:
             self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
 def sniff(host):
-    if os.name == 'nt':
-        socket_protocol = socket.IPPROTO_IP
-    else:
-        socket_protocol = socket.IPPROTO_ICMP
-    
-    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((host, 0)) # double parentheses caus ur pasing (host, port) as a touple
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    
-    if os.name == 'nt':
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-    
+    hosts_up = set([f'{str(self.host)}*'])
+        
     try: # loop to continually read incomming packets and parse their info
         while True:
             # read packet
-            raw_buffer = sniffer.recvfrom(65535)[0]
+            raw_buffer = self.socket.recvfrom(65535)[0]
             
             # create an IP header from the first 20 bytes
             ip_header = IP(raw_buffer[0:20])
             
             if ip_header.protocol == "ICMP": # If protocol ICMP → decode
-                print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
-                print(f'Version: {ip_header.ver}')
-                print(f'Header Length: {ip_header.ihl} TTL: {ip_header.ttl}')
-                
+                                
                 # Calculate where the ICMP starts and create a buffer
                 offset = ip_header.ihl * 4 # Length calculate the offset in the raw packet based on the ip_header length
-                # ihl indicates the number of 32-bit words (4-byte chuncks in the IP)
-                # By multiplying by 4 we get the size of the IP, so where the next network layer begins (ICMP in this case)
                 buf = raw_buffer[offset:offset + 8]
                 # Create ICMP structure
                 icmp_header = ICMP(buf)
-                print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
-            else:
-                # print the detected protocol and hosts
-                print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+                
+                # check for type and code 3
+                if icmp_header.code == 3 and icmp_header.type == 3:
+                    if ipaddress.ip_address(ip_header.src_address) in ipaddress.IPv4Network(SUBNET):
+                        
+                        #Make sure it is responding to 𝘮𝘺 UDP message
+                        if raw_buffer[len(raw_buffer) - len(MESSAGE)] == bytes(MESSAGE, 'utf8'):
+                            tgt = str(ip_header.src_address)
+                            if tgt!=self.host and tgt not in hosts_up:
+                                hosts_up.add(str(ip_header.src_address))
+                                print(f'Host Up: {tgt}') 
+                    
     except KeyboardInterrupt:
         # if on windows, turn off promiscuous mode
         if os.name=='nt':
-            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        
+        print('\nUser interrupted.')
+        if hosts_up:
+            print(f'\n\nSummary: Hosts up on {SUBNET}')
+            for host in sorted(hosts_up):
+                print(f'{host}')
+                print(' ')
         sys.exit()
         
 if __name__=='__main__':
