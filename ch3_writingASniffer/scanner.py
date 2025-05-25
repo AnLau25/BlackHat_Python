ffb1a77936e7cdefbbebf59cc6a7b574cf262e87
AnLau25 <avald024@uottawa.ca>
2025-05-25 12:57:08 -0400

    sniff -> Scanner

--- ch3_writingASniffer/scanner.py
+++ ch3_writingASniffer/scanner.py
@@ -67,48 +67,48 @@ class Scanner:
         if os.name == 'nt':
             self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-def sniff(host):
-    hosts_up = set([f'{str(self.host)}*'])
-        
-    try: # loop to continually read incomming packets and parse their info
-        while True:
-            # read packet
-            raw_buffer = self.socket.recvfrom(65535)[0]
+    def sniff(self):
+        hosts_up = set([f'{str(self.host)}*'])
             
-            # create an IP header from the first 20 bytes
-            ip_header = IP(raw_buffer[0:20])
-            
-            if ip_header.protocol == "ICMP": # If protocol ICMP → decode
-                                
-                # Calculate where the ICMP starts and create a buffer
-                offset = ip_header.ihl * 4 # Length calculate the offset in the raw packet based on the ip_header length
-                buf = raw_buffer[offset:offset + 8]
-                # Create ICMP structure
-                icmp_header = ICMP(buf)
+        try: # loop to continually read incomming packets and parse their info
+            while True:
+                # read packet
+                raw_buffer = self.socket.recvfrom(65535)[0]
                 
-                # check for type and code 3
-                if icmp_header.code == 3 and icmp_header.type == 3:
-                    if ipaddress.ip_address(ip_header.src_address) in ipaddress.IPv4Network(SUBNET):
-                        
-                        #Make sure it is responding to 𝘮𝘺 UDP message
-                        if raw_buffer[len(raw_buffer) - len(MESSAGE)] == bytes(MESSAGE, 'utf8'):
-                            tgt = str(ip_header.src_address)
-                            if tgt!=self.host and tgt not in hosts_up:
-                                hosts_up.add(str(ip_header.src_address))
-                                print(f'Host Up: {tgt}') 
+                # create an IP header from the first 20 bytes
+                ip_header = IP(raw_buffer[0:20])
+                
+                if ip_header.protocol == "ICMP": # If protocol ICMP → decode
+                                    
+                    # Calculate where the ICMP starts and create a buffer
+                    offset = ip_header.ihl * 4 # Length calculate the offset in the raw packet based on the ip_header length
+                    buf = raw_buffer[offset:offset + 8]
+                    # Create ICMP structure
+                    icmp_header = ICMP(buf)
                     
-    except KeyboardInterrupt:
-        # if on windows, turn off promiscuous mode
-        if os.name=='nt':
-            self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-        
-        print('\nUser interrupted.')
-        if hosts_up:
-            print(f'\n\nSummary: Hosts up on {SUBNET}')
-            for host in sorted(hosts_up):
-                print(f'{host}')
-                print(' ')
-        sys.exit()
+                    # check for type and code 3
+                    if icmp_header.code == 3 and icmp_header.type == 3:
+                        if ipaddress.ip_address(ip_header.src_address) in ipaddress.IPv4Network(SUBNET):
+                            
+                            #Make sure it is responding to 𝘮𝘺 UDP message
+                            if raw_buffer[len(raw_buffer) - len(MESSAGE)] == bytes(MESSAGE, 'utf8'):
+                                tgt = str(ip_header.src_address)
+                                if tgt!=self.host and tgt not in hosts_up:
+                                    hosts_up.add(str(ip_header.src_address))
+                                    print(f'Host Up: {tgt}') 
+                        
+        except KeyboardInterrupt:
+            # if on windows, turn off promiscuous mode
+            if os.name=='nt':
+                self.socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            
+            print('\nUser interrupted.')
+            if hosts_up:
+                print(f'\n\nSummary: Hosts up on {SUBNET}')
+                for host in sorted(hosts_up):
+                    print(f'{host}')
+                    print(' ')
+            sys.exit()
         
 if __name__=='__main__':
     if len(sys.argv) == 2:
