e82f921c181b0aa8053ba38f4d544864e3e40c15
AnLau25 <avald024@uottawa.ca>
2025-05-26 13:53:14 -0400

    cmntd

---
+++ ch4_owningTheNetworkWithScapy/mail_sniffer.py
@@ -0,0 +1,7 @@
+# The modern scapy lib runs on wondows tho fue pensada para Linux
+# Estos programas funcionan, suponinedo que que uno se infiltró al LAN del target
+# Esto es un sniffer para 𝗦𝗶𝗺𝗽𝗹𝗲 𝗠𝗮𝗶𝗹 𝗧𝗿𝗮𝗻𝘀𝗽𝗼𝗿𝘁 𝗣𝗿𝗼𝘁𝗼𝗰𝗼𝗹 (𝗦𝗠𝗧𝗣), 𝗣𝗼𝘀𝘁 𝗢𝗳𝗳𝗶𝗰𝗲 𝗣𝗿𝗼𝘁𝗼𝗰𝗼𝗹 (𝗣𝗢𝗣𝟯) 𝘆 𝗜𝗻𝘁𝗲𝗿𝗻𝗲𝘁 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗔𝗰𝗰𝗲𝘀 𝗣𝗿𝗼𝘁𝗼𝗰𝗼𝗹 (𝗜𝗠𝗔𝗣)
+# Si a esto se le añade el 𝗔𝗱𝗱𝗿𝗲𝘀 𝗥𝗲𝘀𝗼𝗹𝘂𝘁𝗶𝗼𝗻 𝗣𝗿𝗼𝘁𝗼𝗰𝗼𝗹 (𝗔𝗥𝗣)y el ataque poissoning-main-in-the-middle (MITM) cara robar credenciales
+# La tecnica funciona para cualquier protocolo pero lo vamos a usar para esto, de momento
+
+
