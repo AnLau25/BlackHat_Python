d7f2fd78f638566ad644a6857df3403070cad429
AnLau25 <avald024@uottawa.ca>
2025-05-27 17:09:44 -0400

    run

--- ch4_owningTheNetworkWithScapy/arper.py
+++ ch4_owningTheNetworkWithScapy/arper.py
@@ -15,17 +15,35 @@ from scapy.all import (ARP, Ether, conf, get_if_hwaddr, send, sniff, sndrcv, srp
 
 def get_mac(targetship):
     packet = Ether(dst = 'ff:ff:ff:ff:ff:ff')/ARP(op="who-has", pdst=targetship)
+    # Pass target ip to create a packet, 𝘌𝘵𝘩𝘦𝘳 𝘧𝘰𝘳 𝘣𝘳𝘰𝘢𝘥𝘤𝘢𝘴𝘵 and 𝘈𝘙𝘗 𝘵𝘰 𝘳𝘦𝘲𝘶𝘦𝘴𝘵 𝘔𝘈𝘊 
     resp, _ = srp(packet, timeout=2, retry=0, verbose=False)
+    # pass packet onto srp to send on Nerwork level 2 (it also receives)
     for _, r in resp:
         return r[Ether].src
     return None 
 
 class Arper:
     def __init__(self, victim, gateway, interface = 'eth0'):
-        pass
+        self.victim = victim
+        self.victimMac = get_mac(victim)
+        self.gateway = gateway
+        self.gatewayMac = get_mac(gateway)
+        self.interface = interface
+        conf.iface = interface
+        conf.verb = 0 
+        # initialize the class with the victim and gateway to get their MAC. Specify interface (eth0 as default)
+        print(f'Initialized {interface}:')
+        print(f'Gateway ({gateway}) is at {self.gatewayMac}')
+        print(f'Victim ({victim}) is at {self.victimMac}')
+        print('-'*30)
     
     def run(self):
-        pass
+        self.poison_thread = Process(target=self.poison)
+        self.poison_thread.start()
+        # Poison start to unleach attack
+        self.sniff_thread = Process(target=self.sniff)
+        self.sniff_thread.start()
+        # Sniff start to track attack by network sniffing
     
     def poison(self):
         pass
