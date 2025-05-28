5a8cc18680794430c6d0fb2e684133c561a86e81
AnLau25 <avald024@uottawa.ca>
2025-05-28 18:37:22 -0400

    struct

--- ch4_owningTheNetworkWithScapy/recapper.py
+++ ch4_owningTheNetworkWithScapy/recapper.py
@@ -8,3 +8,30 @@ import sys
 import zlib
 from collections import namedtuple # Like a tupple, but u can call it by name; inits with ntuple = nametuple('ntuple', ['Name1', 'Name2']) 
                                    # ∴ you can call ntuple.Name1 and ntuple.Name2 instead of indexes
+
+OUTDIR = '/root/Desktop/pictures' # To be determined
+PCAPS = '/root/Downloads' # To be determined
+
+# contains attributes
+Response = namedtuple('Response', ['header', 'payload'])
+
+def get_header(payload):
+    pass
+
+def extract_content(Response, content_name='image'):
+    pass
+
+class Recapper:
+    def __init__(self, fname):
+        pass
+    def get_responses(self):
+        pass
+    
+    def write(self, content_name):
+        pass
+
+if __name__ == '__main__':
+    pfile = os.path.join(PCAPS, 'pcap.pcap')
+    recapper = Recapper(pfile)
+    recapper.get_responses()
+    recapper.write('image')
