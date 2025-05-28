eb13df96c519d790bdab65aaeae5ab7f075d6c71
AnLau25 <avald024@uottawa.ca>
2025-05-28 18:28:08 -0400

    import

--- ch4_owningTheNetworkWithScapy/recapper.py
+++ ch4_owningTheNetworkWithScapy/recapper.py
@@ -1,4 +1,10 @@
 # Analyzes the pcap file from arper.py 
 # Locates any imagein the stream
 # Writes the image to the disk
- 
+
+import os
+import re
+import sys
+import zlib
+from collections import namedtuple # Like a tupple, but u can call it by name; inits with ntuple = nametuple('ntuple', ['Name1', 'Name2']) 
+                                   # ∴ you can call ntuple.Name1 and ntuple.Name2 instead of indexes
