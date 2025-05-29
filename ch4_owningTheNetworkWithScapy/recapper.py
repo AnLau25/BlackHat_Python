8fb2c8d080bf5df32ba4428d5b74f9c408f5fe17
AnLau25 <avald024@uottawa.ca>
2025-05-28 22:45:21 -0400

    cmntd

--- ch4_owningTheNetworkWithScapy/recapper.py
+++ ch4_owningTheNetworkWithScapy/recapper.py
@@ -45,45 +45,51 @@ def extract_content(Response, content_name='image'):
             if Response.header['Content-Encoding'] == "gzip": 
                 content = zlib.decompress(Response.payload, zlib.MAX_WBITS|32) # Dcompres
             elif Response.header['Content-Encoding'] == 'deflate':
-                content = zlib.decompress(Response.payload) # Dcompres
+                content = zlib.decompress(Response.payload) # Decompres
                  
     return content, content_type
 
 class Recapper:
     def __init__(self, fname):
-        pcap = rdpcap(fname)
-        self.sessions = pcap.sessions()
-        self.responses = list()
+        pcap = rdpcap(fname) # init with the name of the pcap
+        self.sessions = pcap.sessions() # Use Scapy to automatically turn TCP into dictionary
+        self.responses = list() # Empty list to capture .pcap responses
         
     def get_responses(self):
-        for session in self.sessions:
+        for session in self.sessions: # iterate over sesion dictionary
             payload = b''
-            for packet in self.sessions[session]:
+            for packet in self.sessions[session]: # iterate over packet in each sesion
                 try:
-                    if packet[TCP].dport == 80 or packet[TCP].sport == 80:
-                        payload += bytes(packet[TCP].payload)
-                except IndexError:
+                    if packet[TCP].dport == 80 or packet[TCP].sport == 80: # filter to get only destination port 80
+                        payload += bytes(packet[TCP].payload) # Concatenate trafic into a single buffer
+                except IndexError: # if no payload (probably cause no TCP) print 'x'
                     sys.stdout.write('x')
                     sys.stdout.flush()
                     
-            if payload:
+            if payload: # If load not empty, pass to get_header to inspect HTTP individually
                 header = get_header(payload)
                 if header is None:
                     continue
-                self.responses.append(Response(header=header, payload=payload))
+                self.responses.append(Response(header=header, payload=payload)) # Append response to responses[]
                 
     
-    def write(self, content_name):
-        for i, response in enumerate(self.responses):
-            content, content_type = extract_content(response, content_name)
+    def write(self, content_name): 
+        for i, response in enumerate(self.responses): # itearte over respnses[]
+            content, content_type = extract_content(response, content_name) # Extract content
             if content and content_type:
-                fname = os.path.join(OUTDIR, f'ex_{i}.{content_type}')
+                fname = os.path.join(OUTDIR, f'ex_{i}.{content_type}') # Ex.img name ex_2.jpg
+                # Name is formed ussing the 𝘦𝘯𝘶𝘮𝘦𝘳𝘢𝘵𝘦 𝘧𝘶𝘯𝘤𝘵𝘪𝘰𝘯 and the 𝘤𝘰𝘯𝘵𝘦𝘯𝘵_𝘵𝘺𝘱𝘦 𝘷𝘢𝘭𝘶𝘦  
                 print(f'Writing {fname} Ɛ( · — ·)3')
                 with open(fname, 'wb') as f:
-                    f.write(content)
+                    f.write(content) # write content to file
 
 if __name__ == '__main__':
     pfile = os.path.join(PCAPS, 'pcap.pcap')
     recapper = Recapper(pfile)
     recapper.get_responses()
     recapper.write('image')
+
+# When run: 
+# 1. Create a Recaper obj
+# 2. Call get_responses 
+# 3. Write extracted response imgs to disk 
