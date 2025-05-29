fe4b948e0c6c80bee8619281dfe43c71561936a0
AnLau25 <avald024@uottawa.ca>
2025-05-29 17:51:12 -0400

    struktur

--- ch4_owningTheNetworkWithScapy/detector.py
+++ ch4_owningTheNetworkWithScapy/detector.py
@@ -1,3 +1,16 @@
 # We gots face recognition technologey 	ψ( ` ∇ ´ )ψ
 # Will go through the results of scaning pcap for images and identify those his human faces on them
 # After, it will generate a new image, with a box sorrounding all faces identified in the img
+
+import cv2
+import os
+
+ROOT = '/root/Desktop/pictures'
+FACES = '/root/Desktop/faces'
+TRAIN = '/root/Desktop/training'
+
+def detect(srcdir=ROOT, tgtdir=FACES, train_dir=TRAIN):
+    pass
+
+if __name__=='__main__':
+    detect()
