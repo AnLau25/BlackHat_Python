8615204a9120d2ae23b1ce5b68c82e3ed2e2f51e
AnLau25 <avald024@uottawa.ca>
2025-05-29 19:10:10 -0400

    i find myself  funy

--- ch4_owningTheNetworkWithScapy/detector.py
+++ ch4_owningTheNetworkWithScapy/detector.py
@@ -1,4 +1,5 @@
 # We gots face recognition technologey 	ψ( ` ∇ ´ )ψ
+# Face rrrrecognition teCHnology!? InalifT!? In Scotlend!? ಠ_ಠ
 # Will go through the results of scaning pcap for images and identify those his human faces on them
 # After, it will generate a new image, with a box sorrounding all faces identified in the img
 
