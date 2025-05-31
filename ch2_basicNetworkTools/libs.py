4bc25245ca49ccb2f55650aae0a9c67594c78645
AnLau25 <avald024@uottawa.ca>
2025-05-30 21:50:42 -0400

    urllib3

--- ch2_basicNetworkTools/libs.py
+++ ch2_basicNetworkTools/libs.py
@@ -18,6 +18,24 @@
 # response.close() 
 
 import urllib.parse
-import urllib.response
+import urllib.request
 
-url = 'https://www.nostarch.com'
+url = 'http://boodelyboo.com' # url definition for http req
+with urllib.request.urlopen(url) as response: # urllib as context manager for request
+    content = response.read() # Read response
+print()
+
+# to create a POST request, pass de data dictionary to the request obj, encoded
+# the dictionary in question should have key value pairs targeting web app aspects
+info = {'user' : 'Name', 'passwd':'1407'}
+data =  urllib.parse.urlencode(info).encode() # data type -> byte
+
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response: # POST
+# Sends the credentials in byte type and records the web response to the logIn attempt
+    content = response.read()
+print(content)
+
+
+
+
