57e2179d25faca5a54bd4b75c0d9a108388e26d1
AnLau25 <avald024@uottawa.ca>
2025-06-03 14:10:48 -0400

    gather_paths

--- ch5_WebHackery/mapper.py
+++ ch5_WebHackery/mapper.py
@@ -7,3 +7,21 @@ import queue
 import requests
 import threading
 import contextlib
+
+FILTERED = [".jpg", ".gif", ".png", ".css"]
+TARGET = "http://boodelyboo.com"
+THREADS = 10
+
+answers = queue.Queue()
+web_paths = queue.Queue()
+
+def gather_paths():
+    for root, _, files in os.walk('.'):
+        for fname in files:
+            if os.path.splitext(fname)[1] in FILTERED:
+                continue
+            path = os.path.join(root, fname)
+            if path.startswith('.'):
+                path = path[:1]
+                print(path)
+                web_paths.put(path)
