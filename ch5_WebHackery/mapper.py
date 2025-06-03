0b53da1677dddc4a4c23c91b9e705b806e393aeb
AnLau25 <avald024@uottawa.ca>
2025-06-03 13:59:12 -0400

    import

--- ch5_WebHackery/mapper.py
+++ ch5_WebHackery/mapper.py
@@ -1,2 +1,9 @@
 # Gaining acces and exploiting Content Management Systems, for human made error or bugs (Wordpress for example)
- 
+
+import os
+import sys
+import time
+import queue
+import requests
+import threading
+import contextlib
