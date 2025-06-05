16edad5b74392e16100feaea1b70d874930865bc
AnLau25 <avald024@uottawa.ca>
2025-06-05 13:37:31 -0400

    bruter.py

---
+++ ch5_WebHackery/bruter.py
@@ -0,0 +1,5 @@
+# acc thought it said butter, anyway Ɛ( · — ·)3
+# Normalmente lanzarias una araña como la de Burp Suit, para "crawl" a travez delos files
+# Pero, a veces, lo que quires agarrar son ficheros de configuracion, debugging o restos del dev que no c usaron
+# En ese caso puedes usar fuerza brta ara encontrarlos por medio de nombres, como el projecto Gobuster o SVNDigger
+# Descargarel .zip de SVNDigger de github y encontrar all.txt 
