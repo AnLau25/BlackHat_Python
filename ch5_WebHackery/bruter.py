837106da8a0d924c898b276e593d1b0400c7cc3a
AnLau25 <avald024@uottawa.ca>
2025-06-05 13:38:18 -0400

    import

--- ch5_WebHackery/bruter.py
+++ ch5_WebHackery/bruter.py
@@ -3,3 +3,9 @@
 # Pero, a veces, lo que quires agarrar son ficheros de configuracion, debugging o restos del dev que no c usaron
 # En ese caso puedes usar fuerza brta ara encontrarlos por medio de nombres, como el projecto Gobuster o SVNDigger
 # Descargarel .zip de SVNDigger de github y encontrar all.txt 
+
+import sys
+import queue
+import requests
+import threading
+
