c14c144814137de694fb844c66c4effe72506124
AnLau25 <avald024@uottawa.ca>
2025-06-10 17:14:14 -0400

    notes

---
+++ ch6_ExtendingBurpProxy/Notes.py
@@ -0,0 +1,6 @@
+# 𝗦𝗲𝘁 𝘂𝗽 𝗳𝗼𝗿 𝗣𝘆𝘁𝗵𝗼𝗻:
+# Download 𝘑𝘺𝘵𝘩𝘰𝘯 𝘚𝘵𝘢𝘯𝘥𝘢𝘭𝘰𝘯𝘦 JAR⁡⁢⁣⁢*⁡
+# Go to Extensions >>> Extensions Settings >>> Python env. >>> Location of Jython standalone JAR file 
+# Place file location in box
+# Close and re-open
+# ​‍‌‌⁡⁢⁣⁢‍‍1⁡​​Jython expects python2 
