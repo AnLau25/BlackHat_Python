e79208040ca34e07edcfbfb8a8c05c52a7091c53
AnLau25 <avald024@uottawa.ca>
2025-06-09 22:24:59 -0400

    requirements

--- ch5_WebHackery/wordpress_killer.py
+++ ch5_WebHackery/wordpress_killer.py
@@ -1,5 +1,12 @@
 # Brute forcing of POST request 
 # For testing form filling, captcha, log-in token or any type of submition 
+# 𝗥𝗲𝗾𝘂𝗶𝗿𝗲𝗺𝗲𝗻𝘁𝘀: 
+#   - Retreive hidden token from before pswd submition attemps 
+#   - Ensure http session accepts cookies
+# The remote app sets one or more cookies on on contact and expects them back on login
+# 𝘉𝘦𝘢𝘶𝘵𝘪𝘧𝘶𝘭𝘚𝘰𝘶𝘱 𝘢𝘯𝘥 𝘭𝘹𝘮𝘭 packages to parse login form values
+# Worpress checks the cookies againts the current user session
+# If the cookies don't mach, the login will fail even with the right creds
 
   
 
