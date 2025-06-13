03586f55f1f474855d52309424b1a325a0adfb5c
AnLau25 <avald024@uottawa.ca>
2025-06-13 15:36:07 -0400

    registerExtenderCallbacks

--- ch6_ExtendingBurpProxy/bhp_fuzzer.py
+++ ch6_ExtendingBurpProxy/bhp_fuzzer.py
@@ -5,4 +5,12 @@ from burp import IIntruderPayloadGeneratorFactory
 import random
 from java.util import List, ArrayList
 
-
+class BurpExtender(IBrupExtender, IIntruderPayloadGeneratorFactory):
+    
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callable
+        self._helpers = callbacks.getHelpers()
+        
+        callbacks.registerIntruderPayloadGeneratorFactory(self)
+        
+        return
