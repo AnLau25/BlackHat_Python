a0373e72fcc80f8ad1a6acf8f827d1c7b41bdcbd
AnLau25 <avald024@uottawa.ca>
2025-06-13 15:33:48 -0400

    import

---
+++ ch6_ExtendingBurpProxy/bhp_fuzzer.py
@@ -0,0 +1,8 @@
+from burp import IBrupExtender
+from burp import IIntruderPayloadGenerator
+from burp import IIntruderPayloadGeneratorFactory
+
+import random
+from java.util import List, ArrayList
+
+
