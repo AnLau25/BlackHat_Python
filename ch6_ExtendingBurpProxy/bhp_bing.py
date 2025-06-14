ffb9539f1e1a5af16099f8c2f1ebb2efaefc75f5
AnLau25 <avald024@uottawa.ca>
2025-06-14 13:35:05 -0400

    import

--- ch6_ExtendingBurpProxy/bhp_bing.py
+++ ch6_ExtendingBurpProxy/bhp_bing.py
@@ -1,1 +1,15 @@
 # Bing allows you to query for for all the websites it finds at an IP using, the "IP" search modifier
+
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+
+from java.net import URL
+from java.util import ArrayList
+from javax.swing import JMenuItem
+from thread import start_new_thread
+
+import json
+import socket
+import urllib
+API_KEY = "YOURKEY"
+API_HOST = 'api.cognitive.microsoft.com'
