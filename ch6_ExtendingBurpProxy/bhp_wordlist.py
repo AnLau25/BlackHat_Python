b417b73a7b72c05ed69ccad6801aecb4b02225ea
AnLau25 <avald024@uottawa.ca>
2025-06-16 21:45:54 -0400

    bhp_wordlist

---
+++ ch6_ExtendingBurpProxy/bhp_wordlist.py
@@ -0,0 +1,11 @@
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+
+from java.util import ArrayList
+from javax.swing import JMenuItem
+
+from datetime import datetime
+from HTMLParser import HTMLParser
+
+import re
+
