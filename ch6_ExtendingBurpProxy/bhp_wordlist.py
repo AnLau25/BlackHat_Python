55f242339e6bc63131493bf60fc5ca3caaa4d688
AnLau25 <avald024@uottawa.ca>
2025-06-16 21:55:50 -0400

    class BurpExtender

--- ch6_ExtendingBurpProxy/bhp_wordlist.py
+++ ch6_ExtendingBurpProxy/bhp_wordlist.py
@@ -25,4 +25,26 @@ class TagStripper(HTMLParser):
         self.feed(html)
         return " ".join(self.page_text)
 
+class BurpExtender(IBurpExtender,IContextMenuFactory):
     
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        self.context = None
+        self.hosts = set()
+    
+        # Start with something common/espected
+        self.wordlist = set(["pasword"])
+        
+        # set up extension
+        callbacks.setExtenxionName("BHP Wordlist")
+        callbacks.registerContextMenuFactory(self)
+        
+        return
+    
+    def createMenuItems(self, context_menu):
+        self.context = context_menu
+        menu_list = ArrayList()
+        menu_list.add(JMenuItem(
+            "Create Wordlist", actionPerformed = self.wordlist_menu))
+        return menu_list
