755570b99ea65f06a4e5602d217d33f573319755
AnLau25 <avald024@uottawa.ca>
2025-06-16 21:49:28 -0400

    class TagStripper

--- ch6_ExtendingBurpProxy/bhp_wordlist.py
+++ ch6_ExtendingBurpProxy/bhp_wordlist.py
@@ -9,3 +9,20 @@ from HTMLParser import HTMLParser
 
 import re
 
+class TagStripper(HTMLParser):
+    
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.page_text = []
+    
+    def handle_data(self, data):
+        self.page_text.append(data)
+    
+    def handle_comment(self, data):
+        self.page_text.append(data)
+    
+    def strip(self, html):
+        self.feed(html)
+        return " ".join(self.page_text)
+
+    
