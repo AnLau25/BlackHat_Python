045d12c184014e5348e9f4ee0d7e72a579b2ca40
AnLau25 <avald024@uottawa.ca>
2025-06-24 12:31:21 -0400

    imports

--- ch8_CommonTrojaningTasksOnWindows/screenshotter.py
+++ ch8_CommonTrojaningTasksOnWindows/screenshotter.py
@@ -1,2 +1,8 @@
 # Uses the 𝘞𝘪𝘯𝘥𝘰𝘸𝘴 𝘎𝘳𝘢𝘱𝘩𝘪𝘤𝘴 𝘋𝘦𝘷𝘪𝘤𝘦 𝘐𝘯𝘵𝘦𝘳𝘧𝘢𝘤𝘦 to determine the screen properties
 # This gives us teh advantage of getting a full-screen pictures
+
+import base64
+import win32api
+import win32con
+import win32gui
+import win32ui
