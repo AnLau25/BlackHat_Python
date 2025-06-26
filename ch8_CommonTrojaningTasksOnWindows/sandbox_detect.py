977e9d659f080c5b1ac1af742f1128588c487d17
AnLau25 <avald024@uottawa.ca>
2025-06-26 13:31:09 -0400

    Detector

--- ch8_CommonTrojaningTasksOnWindows/sandbox_detect.py
+++ ch8_CommonTrojaningTasksOnWindows/sandbox_detect.py
@@ -25,7 +25,27 @@ def get_last_input(): # Determine the last time of inut
     
     return elapsed
 
-while True: # Tester
-    get_last_input()
-    time.sleep(1)
+#while True: # Tester
+#    get_last_input()
+#    time.sleep(1)
 
+class Detector:
+    def __init__(self):
+        self.double_clicks = 0
+        self.keystrokes = 0
+        self.mouse_clicks = 0
+    
+    def get_key_press(self):
+        for i in range (0, 0xff):
+            state = win32api.GetAsynKeyState(i)
+            
+            if state & 0x0001:
+            
+                if i == 0x1:
+                    self.mouse_clicks += 1
+                    return time.time()
+            
+                elif i>32 and i<127:
+                    self.keystrokes += 1
+        
+        return None
