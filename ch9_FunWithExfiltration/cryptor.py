0da457c6f20a51335f9c01229450046baa3fe1f5
AnLau25 <avald024@uottawa.ca>
2025-06-27 14:04:08 -0400

    decrypt(encrypted)

--- ch9_FunWithExfiltration/cryptor.py
+++ ch9_FunWithExfiltration/cryptor.py
@@ -26,15 +26,39 @@ def get_rsa_cipher(keytype): # Pass pub or pri to get the keys
 
 def encrypt(plaintext):
     compressed_text = zlib.compress(plaintext)
+    # Compress plaintext
     
-    session_key =  get_random_bytes(16)
+    session_key =  get_random_bytes(16) # Generate random sess key
     cipher_aes = AES.new(session_key, AES.MODE_EAX)
     ciphertext, tag = cipher_aes.encrypt_and_digest(compressed_text)
+    # Encrypt compressed plain text
     
     cipher_rsa, _ = get_rsa_cipher('pub')
     encrypted_session_key = cipher_rsa.encrypt(session_key)
+    # Encrypt the RSA key generated from the generated public key
     
     msg_payload = encrypted_session_key + cipher_aes.nonce + tag + ciphertext
+    # Pull all the needed info into one single payload
     encrypted = base64.encodebytes(msg_payload)
+    # base64 encode the payload
     return encrypted 
+
+def decrypt(encrypted): # Lit the reverse of encrypt (Walking backwards)
+    encrypted_bytes = BytesIO(base64.decodebytes(encrypted)) # de-base64
+    cipher_rsa, keysize_in_bytes = get_rsa_cipher('pri') 
+    
+    # Read encrypted session key & other params
+    encrypted_session_key = encrypted_bytes.read(keysize_in_bytes)
+    nonce = encrypted_bytes.read(16)
+    tag = encrypted_bytes.read(16)
+    ciphertext = encrypted_bytes.read()
+    
+    # Decrypt key with RSA priv
+    session_key = cipher_rsa.decrypt(encrypted_session_key)
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    # Decrypt with AES cipher
+    decrypted = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    
+    plaintext = zlib.decompress(decrypted) # Decrypt plain text
+    return plaintext
 
