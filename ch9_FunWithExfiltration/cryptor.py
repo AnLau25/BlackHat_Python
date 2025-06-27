46e7b1f73807fbe51c8adaaaf87a478c7b25fe19
AnLau25 <avald024@uottawa.ca>
2025-06-27 13:28:40 -0400

    cryptor

---
+++ ch9_FunWithExfiltration/cryptor.py
@@ -0,0 +1,7 @@
+from Cryptodome.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from io import BytesIO
+
+import base64
+import zlib
