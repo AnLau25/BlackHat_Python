d5c0c7fb9f6713b1cd87b48bee2a893a88f1fddb
AnLau25 <avald024@uottawa.ca>
2025-06-28 13:57:20 -0400

    imports

--- ch9_FunWithExfiltration/email_exfil.py
+++ ch9_FunWithExfiltration/email_exfil.py
@@ -1,1 +1,13 @@
 # Exfiltrate encrypted information
+
+import smtplib # for cross-platform e-mail funct
+import time 
+import win32com.client # for win specific functs
+
+smtp_server = 'smtp.example.com' # Connect to SMTP (Simple Mail Transfer Protocol Server)
+smtp_port = 587 
+smtp_acct = 'an@example.com'
+smtp_password = 'seKret'
+tgt_accts = ['an@elsewhere.com']
+
+
