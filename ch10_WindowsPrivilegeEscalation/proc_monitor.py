5b7b9d1db921124675c83ed498021642dc06b1c3
AnLau25 <avald024@uottawa.ca>
2025-07-04 12:03:59 -0400

    log_to_file(message)

--- ch10_WindowsPrivilegeEscalation/proc_monitor.py
+++ ch10_WindowsPrivilegeEscalation/proc_monitor.py
@@ -6,3 +6,9 @@ import win32api
 import win32con
 import win32security
 import wmi
+
+def log_to_file(message):
+    with open('process_monitor_log.scv', 'a') as fd:
+        fd.write(f'{message}\r\n')
+        
+    
