3758b690ae84ca00481c24820906231aed9fc5be
AnLau25 <avald024@uottawa.ca>
2025-07-04 12:13:40 -0400

    monitor()

--- ch10_WindowsPrivilegeEscalation/proc_monitor.py
+++ ch10_WindowsPrivilegeEscalation/proc_monitor.py
@@ -10,5 +10,33 @@ import wmi
 def log_to_file(message):
     with open('process_monitor_log.scv', 'a') as fd:
         fd.write(f'{message}\r\n')
+
+def monitor():
+    head = 'CommandLine, Time, Executable, Parent PID, PID, User, Privileges'
+    log_to_file(head)
+    c = wmi.WMI()
+    process_watcher = c.Win32_Process.watch_for('creation')
+    
+    while True:    
+        try:
+            new_process = process_watcher()
+            cmndline = new_process.CommandLine
+            create_date = new_process.CreationDate
+            executable = new_process.ExecutablePath
+            parent_pid = new_process.ParentProcessId
+            pid = new_process.ProcessId
+            proc_owner = new_process.GetOwner()
+            
+            privileges = 'N/A'
+            process_log_message = (
+                f'{cmndline}, {create_date}, {executable}',
+                f'{parent_pid}, {pid}, {proc_owner}, {privileges}'
+            )
+            
+            print(process_log_message)
+        except Exception:
+            pass
+        
+    
         
     
