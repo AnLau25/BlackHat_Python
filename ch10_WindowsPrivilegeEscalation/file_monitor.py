4bd24923d4d58dcc4492886e522e33206e8af9bf
AnLau25 <avald024@uottawa.ca>
2025-07-07 14:10:32 -0400

    monitor

--- ch10_WindowsPrivilegeEscalation/file_monitor.py
+++ ch10_WindowsPrivilegeEscalation/file_monitor.py
@@ -12,3 +12,54 @@ FILE_RENAMED_TO = 5
 
 FILE_LIST_DIR = 0x0001
 PATHS = ['C:\\WINDOWS\\Temp', tempfile.gettempdir()]
+
+def monitor(path_to_watch):
+    h_dir = win32file.CreateFile(
+        path_to_watch,
+        FILE_LIST_DIR,
+        win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
+        None,
+        win32con.OPEN_EXISTING,
+        win32con.FILE_FLAG_BACKUP_SEMANTICS,
+        None
+    )
+    while True:
+        try:
+            results = win32file.ReadDirectoryChangesW(
+                h_dir,
+                1024,
+                True,
+                win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+                win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+                win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+                win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+                win32con.FILE_NOTIFY_CHANGE_SECURITY |
+                win32con.FILE_NOTIFY_CHANGE_SIZE,
+                None,
+                None                
+            )
+            for action, file_name in results:
+                full_filename = os.path.join(path_to_watch, file_name)
+                if action == FILE_CREATED:
+                    print(f'[+] Created {full_filename}')
+                elif action == FILE_DELETED:
+                    print(f'[-] Deleted {full_filename}')
+                elif action == FILE_MODIFIED:
+                    print(f'[*] Modified {full_filename}')
+                    try:
+                        print('[vvv] Dumpting contents...')
+                        with open(full_filename) as f:
+                            contents = f.read()
+                        print(contents)
+                        print('[^^^] Dump complete.')
+                    except Exception as e:
+                        print(f'[!!!] Dump failed. {e}')
+                elif action == FILE_RENAMED_FROM:
+                    print(f'[>] Renamed from {full_filename}')
+                elif action == FILE_RENAMED_TO:
+                    print(f'[<] Renamed to {full_filename}')
+                else:
+                    print(f'[?] Unknown action on {full_filename}')
+        except Exception:
+            pass
+        
